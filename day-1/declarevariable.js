d423b8daadfc757cbd70f4998a8c099300a5a78b
heistruth <victor.edbuoro@gmail.com>
2024-02-07 11:15:20 +0100

    adding day 1 task

---
+++ day-1/declarevariable.js
@@ -0,0 +1,35 @@
+//Declare four variables without assigning values
+let variable1;
+let variable2;
+let variable3;
+let variable4;
+
+
+//Declare four variables with assigned values
+let variable5 = 10;
+let variable6 = "Hello";
+let variable7 = true;
+let variable8 = { key: "value" };
+
+//Declare variables to store your first name, last name, marital status, country, and age in multiple lines
+let firstName = 'John';
+let lastName = 'Doe';
+let maritalStatus = 'Single';
+let country = 'United States';
+let age = 30;
+
+
+//Declare variables to store your first name, last name, marital status, country, and age in a single line
+let firstName2 = 'John', lastName2 = 'Doe', maritalStatus2 = 'Single', country2 = 'United States', age2 = 30;
+
+
+//Declare two variables myAge and yourAge and assign them initial values and log to the browser console
+let myAge = 25;
+let yourAge = 30;
+
+console.log("My age is:", myAge);
+console.log("Your age is:", yourAge);
+
+
+ 
+
